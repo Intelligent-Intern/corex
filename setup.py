c49085c8fbeaf743a20296082e3219d4eba30cb0
Jochen Schultz <jschultz@php.net>
2025-03-30 23:26:55 +0200

    update workflow

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
-"""
-Setup script for the corex package.
-"""
-
 from setuptools import setup, find_packages
 
 setup(
     name="corex",
     version="0.1.0",
-    description="CoreX is a modular, standardized interface framework for Python applications. It provides a consistent API for essential cross-cutting concerns such as storage, caching, messaging, security, databases, event processing, AI, and more.",
     author="Jochen Schultz",
     author_email="js@intelligent-intern.com",
     packages=find_packages(where="src"),
